ec7b479b1c6cfaa45a30eb8f9119b2080960d013
mohitpal071 <mohitpalsingh700@gmail.com>
2025-12-25 15:54:09 +0530

    Improved tickStore

--- REST-Service/internal/store/store.go
+++ REST-Service/internal/store/store.go
@@ -2,48 +2,89 @@ package store
 
 import (
 	"sync"
+	"sync/atomic"
 
 	"gokiteconnect-master/models"
 )
 
-// TickStore holds the latest ticks in memory
+// TickStore holds the latest ticks in memory using atomic values for lock-free reads/writes on existing keys
 type TickStore struct {
-	ticks sync.Map
+	mu    sync.RWMutex
+	ticks map[uint32]*atomic.Value
 }
 
 // Global instance
-var GlobalStore = &TickStore{}
+var GlobalStore = New()
+
+func New() *TickStore {
+	return &TickStore{
+		ticks: make(map[uint32]*atomic.Value),
+	}
+}
 
 // Update processes a binary tick and updates the store
 func (s *TickStore) Update(tickBytes []byte) {
-	// Ideally we parse this.
-	// Since we now have access to the parser in internal/ticker, we could expose it.
-	// OR we use UpdateFromTick which is cleaner if we use OnTick callback.
-	// For now, let's just assume main.go will call UpdateFromTick if it gets a struct,
-	// or we can try to parse it here if we expose ParsePacket.
+	// Not implemented for binary yet, waiting for main.go to push updates
 }
 
 // UpdateFromTick updates the store with a structured tick
+// This method is optimized to be virtually lock-free for existing tokens
 func (s *TickStore) UpdateFromTick(tick models.Tick) {
-	s.ticks.Store(tick.InstrumentToken, tick)
+	// Fast path: Try to get value with separate read lock
+	s.mu.RLock()
+	val, ok := s.ticks[tick.InstrumentToken]
+	s.mu.RUnlock()
+
+	if ok {
+		// Wait-free atomic store
+		val.Store(tick)
+		return
+	}
+
+	// Slow path: New token, need write lock
+	s.mu.Lock()
+	// Double check
+	val, ok = s.ticks[tick.InstrumentToken]
+	if !ok {
+		val = &atomic.Value{}
+		s.ticks[tick.InstrumentToken] = val
+	}
+	s.mu.Unlock()
+
+	// Store value
+	val.Store(tick)
 }
 
 // Get returns the tick for a given token
+// This method is optimized to be virtually lock-free
 func (s *TickStore) Get(token uint32) (models.Tick, bool) {
-	val, ok := s.ticks.Load(token)
+	s.mu.RLock()
+	val, ok := s.ticks[token]
+	s.mu.RUnlock()
+
 	if !ok {
 		return models.Tick{}, false
 	}
 
-	return val.(models.Tick), true
+	// Wait-free atomic load
+	x := val.Load()
+	if x == nil {
+		return models.Tick{}, false
+	}
+	return x.(models.Tick), true
 }
 
 // GetAll returns all stored ticks
 func (s *TickStore) GetAll() map[uint32]models.Tick {
-	result := make(map[uint32]models.Tick)
-	s.ticks.Range(func(key, value interface{}) bool {
-		result[key.(uint32)] = value.(models.Tick)
-		return true
-	})
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	result := make(map[uint32]models.Tick, len(s.ticks))
+	for token, val := range s.ticks {
+		x := val.Load()
+		if x != nil {
+			result[token] = x.(models.Tick)
+		}
+	}
 	return result
 }
